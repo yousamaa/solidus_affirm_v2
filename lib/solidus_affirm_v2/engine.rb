f55437eb880bc677c648ac6199728f30fcf71756
Usama Yousaf <usamayousaf177@gmail.com>
2023-12-25 12:58:08 +0000

    Modifed engine.rb to reslove rails 7.0 autoloading issue.

--- lib/solidus_affirm_v2/engine.rb
+++ lib/solidus_affirm_v2/engine.rb
@@ -10,6 +10,7 @@ module SolidusAffirmV2
     isolate_namespace ::Spree
 
     engine_name 'solidus_affirm_v2'
+    config.autoload_once_paths << "#{root}/app/helpers"
 
     # use rspec for tests
     config.generators do |g|
@@ -17,19 +18,23 @@ module SolidusAffirmV2
     end
 
     initializer "register_solidus_affirm_v2_payment_method", after: "spree.register.payment_methods" do |app|
-      app.config.spree.payment_methods << SolidusAffirmV2::PaymentMethod
+      config.to_prepare do
+        app.config.spree.payment_methods << SolidusAffirmV2::PaymentMethod
+      end
     end
 
     initializer "register_solidus_affirm_v2_configuration", before: :load_config_initializers do |_app|
-      SolidusAffirmV2::Config = SolidusAffirmV2::Configuration.new
+      config.to_prepare do
+        SolidusAffirmV2::Config = SolidusAffirmV2::Configuration.new
+      end
     end
 
     initializer 'register_solidus_affirm_v2_helper_action_controller' do |_app|
       ActiveSupport.on_load :action_controller do |klass|
         next if klass.name == "ActionController::API"
 
-        helper SolidusAffirmV2::AffirmHelper
-      end
+         helper SolidusAffirmV2::AffirmHelper
+              end
     end
   end
 end
